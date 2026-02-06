3ea084d6071a25431b373b934abba2f21b21c196
jonathan <jonathan07@gmail.com>
2026-02-06 10:54:06 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000108980_JonathanAndilinata/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000108980_JonathanAndilinata.week01
+
+fun main (){
+    var name: String = "Jonathan"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
