36615f89a71758b6d4dbbd2f19593303e6844bcf
jonathan <jonathan07@gmail.com>
2026-02-13 16:25:11 +0700

    Week01: (tugas 1) add legacy CircleCalculator code

--- src/oop_00000108980_JonathanAndilinata/week01/CircleCalculator.kt
+++ src/oop_00000108980_JonathanAndilinata/week01/CircleCalculator.kt
@@ -1,13 +1,20 @@
 package oop_00000108980_JonathanAndilinata.week01
 
-fun main() {
-    val radius = 7.0
-    val pi = 3.14
+fun main(args: Array<String>){
+    var radius: Double = 7.0
+    var pi: Double = 3.14
 
-    var area = pi * radius * radius
-    println("Radius: $radius , Area: $area")
+    var area: Double = pi * radius * radius
 
-    println(checkSize(area))
+    println("Radius: " + radius + ", Area " + area)
+
+    checkSize(area)
 }
 
-fun checkSize(area: Double) = if (area>100) "This is a big circle" else "This is a small circle"
+fun checkSize(area: Double){
+    if (area > 100){
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
