76d61196a297b3dfcc0595f59c351d476cbe5e76
jonathan <jonathan07@gmail.com>
2026-02-13 16:30:53 +0700

    week01: (tugas 1) covert checkSize to expression body function

--- src/oop_00000108980_JonathanAndilinata/week01/CircleCalculator.kt
+++ src/oop_00000108980_JonathanAndilinata/week01/CircleCalculator.kt
@@ -1,6 +1,6 @@
 package oop_00000108980_JonathanAndilinata.week01
 
-fun main(args: Array<String>){
+fun main(){
     val radius = 7.0
     val pi = 3.14
 
@@ -8,13 +8,9 @@ fun main(args: Array<String>){
 
     println("Radius: $radius, Area: $area ")
 
-    checkSize(area)
+    println(checkSize(area))
 }
 
-fun checkSize(area: Double){
-    if (area > 100){
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
+
+
