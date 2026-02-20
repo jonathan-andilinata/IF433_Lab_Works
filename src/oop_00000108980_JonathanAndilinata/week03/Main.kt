59c6bf92818f41fcd34857da93401ae37da5200e
jonathan <jonathan07@gmail.com>
2026-02-20 21:43:07 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000108980_JonathanAndilinata/week03/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week03/Main.kt
@@ -1,13 +1,10 @@
 package oop_00000108980_JonathanAndilinata.week03
 
-fun main(){
-    val e = Employee("Budi")
-    e.salary = -1000
-    e.salary = 500000
-    println("Gaji: ${e.salary}")
-
-    e.incresePerformance()
-
-    println("Pajak yang harus dibayar: ${e.tax}")
-
+fun main() {
+    val player = Weapon("kapak", -50)
+    val player2 = Weapon("gergaji", 9999)
+    val player3 = Weapon("pedang",750)
+    println("${player.damages},${player.tiers}")
+    println("${player2.damages},${player2.tiers}")
+    println("${player3.damages},${player3.tiers}")
 }
--- src/oop_00000108980_JonathanAndilinata/week03/Weapon.kt
+++ src/oop_00000108980_JonathanAndilinata/week03/Weapon.kt
@@ -1,4 +1,31 @@
 package oop_00000108980_JonathanAndilinata.week03
 
-class Weapon {
+class Weapon (
+   val name: String,
+   var damage: Int = 100,
+   val tier: String = "Common"
+) {
+    var damages: Int = damage
+      set(value){
+          if (damages < 0){
+              println("Damage Weapom di set negatif, akan ada masalah nanti!")
+              field = 100
+          }
+          else if (damages > 1000){
+              println("Damage tidak boleh lebih dari 1000, di set ke 1000")
+              field = 1000
+          }
+          }
+
+    val tiers: String
+        get() = when {
+            damages > 800 -> "Legendary"
+            damages > 500 -> "Epic"
+            else -> "Common"
+        }
+
+
+    init {
+        damages = damage
+      }
 }
