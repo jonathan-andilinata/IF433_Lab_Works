7e5be93907f7314980eee71fd946d9366d78426a
jonathan <jonathan07@gmail.com>
2026-03-06 13:50:40 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000108980_JonathanAndilinata/week05/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week05/Main.kt
@@ -1,5 +1,13 @@
 package oop_00000108980_JonathanAndilinata.week05
 
 fun main() {
-    val orang = EWallet("ayam",10.00)
+    val orang = EWallet("ayam",50.000)
+    val manusia = CreditCard("sapi",100000.0,100000.0)
+
+    val daftarOrang: List<PaymentMethod> = listOf(orang,manusia)
+
+    for (method in daftarOrang){
+        method.processPayment(75000.0)
+    }
+
 }
