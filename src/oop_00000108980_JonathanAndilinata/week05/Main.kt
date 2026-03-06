5639ee1a42ac68503f11f08c071976de6422476c
jonathan <jonathan07@gmail.com>
2026-03-06 11:43:09 +0700

    week05: (task 2) create asbtract PaymentMethod

--- src/oop_00000108980_JonathanAndilinata/week05/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week05/Main.kt
@@ -1,24 +1,9 @@
 package oop_00000108980_JonathanAndilinata.week05
 
 fun main() {
-    val dosen1 = Dosen("Pax Alex", "0123456")
-    val admin1 = Admin("Bu Siti")
+    val math = MathHelper()
 
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
-
-    println("=== Aktivitas Pegawai ===")
-    for (pegawai in daftarPegawai) {
-        pegawai.bekerja()
-
-        when (pegawai) {
-            is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar()
-            }
-            is Admin ->{
-                println("=> Terdeteksi sebagai admin")
-            }
-        }
-        println("-------------------------")
-    }
+    println(math.hitungLuas(2))
+    println(math.hitungLuas(2,2))
+    println(math.hitungLuas(2.1))
 }
--- src/oop_00000108980_JonathanAndilinata/week05/MathHelper.kt
+++ src/oop_00000108980_JonathanAndilinata/week05/MathHelper.kt
@@ -1,4 +1,15 @@
 package oop_00000108980_JonathanAndilinata.week05
 
 class MathHelper {
+    fun hitungLuas(sisi: Int): Int{
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int{
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jarijari: Double): Double{
+        return 3.14 * jarijari * jarijari
+    }
 }
