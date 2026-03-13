a3c917697e5188d8bac2c515c608eed5c77df8a4
jonathan <jonathan07@gmail.com>
2026-03-13 10:51:55 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000108980_JonathanAndilinata/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000108980_JonathanAndilinata.week06
+
+interface Camera{
+    fun turnOn(){
+        println("lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan")
+    }
+}
