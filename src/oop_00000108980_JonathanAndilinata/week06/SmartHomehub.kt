1afb224f5e6d990fb88c14a08af807ab89fa9cf3
jonathan <jonathan07@gmail.com>
2026-03-13 16:19:55 +0700

    week06: (task) implement advanced smart casting in hub

--- src/oop_00000108980_JonathanAndilinata/week06/SmartHomehub.kt
+++ src/oop_00000108980_JonathanAndilinata/week06/SmartHomehub.kt
@@ -1,4 +1,34 @@
 package oop_00000108980_JonathanAndilinata.week06
 
-class SmartHomehub {
+class SmartHomehub(override val id: String, override val name: String): SmartDevice, Switchable{
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice){
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            when(device) {
+                is Switchable -> device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode(){
+        for (device in devices){
+            when(device){
+                is Recordable ->{ device.startRecord() }
+                is SmartSpeaker -> { device.playMusic("Sirine Peringatan")}
+            }
+        }
+    }
+
+    override fun turnOn(){
+        println("$name dinyalakan")
+    }
+
+    override fun turnOff(){
+        println("$name dimatikan")
+    }
 }
