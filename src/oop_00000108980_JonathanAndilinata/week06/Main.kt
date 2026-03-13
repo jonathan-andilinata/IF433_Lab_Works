191ad7e5ea1abff25d2bdd929d997970c3d12d5d
jonathan <jonathan07@gmail.com>
2026-03-13 16:31:03 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000108980_JonathanAndilinata/week06/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week06/Main.kt
@@ -2,8 +2,15 @@ package oop_00000108980_JonathanAndilinata.week06
 
 
 fun main(){
-    val lampu = SmartSpeaker("1","Ruang Tamu")
+    val lampu = SmartLamp("1","Ruang Tamu")
     val speaker = SmartSpeaker("2","Google Nest Dapur")
     val CCTV = SmartCCTV("3","Ezviz Garasi")
 
+    val hub = SmartHomehub("1","rumah")
+    hub.addDevice(lampu)
+    hub.addDevice(speaker)
+    hub.addDevice(CCTV)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
