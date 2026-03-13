b9dcb15947056a5de8376992ec0fb5bdfd964132
jonathan <jonathan07@gmail.com>
2026-03-13 16:23:06 +0700

    week06: (task) instantiate smart device in main

--- src/oop_00000108980_JonathanAndilinata/week06/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week06/Main.kt
@@ -2,5 +2,8 @@ package oop_00000108980_JonathanAndilinata.week06
 
 
 fun main(){
+    val lampu = SmartSpeaker("1","Ruang Tamu")
+    val speaker = SmartSpeaker("2","Google Nest Dapur")
+    val CCTV = SmartCCTV("3","Ezviz Garasi")
 
 }
