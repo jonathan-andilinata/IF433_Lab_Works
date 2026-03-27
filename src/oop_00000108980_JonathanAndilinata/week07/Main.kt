81697d1e94d5038d73a07154c718aa563bee7dec
jonathan <jonathan07@gmail.com>
2026-03-27 15:06:39 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000108980_JonathanAndilinata/week07/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/Main.kt
@@ -1,14 +1,6 @@
 package oop_00000108980_JonathanAndilinata.week07
 
 fun main (){
-
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    val uiMessage = when(response) {
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan Alert: ${response.message}"
-        is ApiResponse.Loading -> "Tampilkan Spinner"
-    }
-    println(uiMessage)
+    GameManager.startGame()
+    GameManager.startGame()
 }
