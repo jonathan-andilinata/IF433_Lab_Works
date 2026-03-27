64d6ce0764c954d98a2579457d5d0f34d3895b39
jonathan <jonathan07@gmail.com>
2026-03-27 10:47:17 +0700

    week07: implement data class for structural equality

--- src/oop_00000108980_JonathanAndilinata/week07/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/Main.kt
@@ -2,9 +2,9 @@ package oop_00000108980_JonathanAndilinata.week07
 
 fun main (){
 
-    println("\n=== TEST Regular Class ===")
-    val reg1 = RegularUser("Alice",22)
-    val reg2 = RegularUser("Alice",22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
+    println("\n=== TEST Data Class ===")
+    val data1 = RegularUser("Alice",22)
+    val data2 = RegularUser("Alice",22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 }
--- src/oop_00000108980_JonathanAndilinata/week07/UserModels.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/UserModels.kt
@@ -1,5 +1,5 @@
 package oop_00000108980_JonathanAndilinata.week07
 
-class RegularUser(val name: String, val age: Int) {
+data class RegularUser(val name: String, val age: Int) {
 
 }
