c8ea5b3b782240ffe46d51a769cb79e6cd4cff20
jonathan <jonathan07@gmail.com>
2026-03-27 11:25:14 +0700

    welel07: (task) create ItemRarity enum

---
+++ src/oop_00000108980_JonathanAndilinata/week07/GameModels.kt
@@ -0,0 +1,12 @@
+package oop_00000108980_JonathanAndilinata.week07
+
+enum class GameModels(val dropChance: Int) {
+    COMMON(100),
+    UNCOMMON(80),
+    RARE(50),
+    EPIC(30),
+    LEGENDARY(10)
+}
+
+
+
