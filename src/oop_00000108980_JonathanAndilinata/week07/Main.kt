bcadcf5d12cca29f378e9da4b1f8580a2f46141e
jonathan <jonathan07@gmail.com>
2026-03-27 15:44:19 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000108980_JonathanAndilinata/week07/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/Main.kt
@@ -12,5 +12,12 @@ fun main (){
     println("Rarity senjata: ${pedang.item.rarity}")
     println("Durability senjata: ${pedang.durability}")
 
+    val pedangBaru = pedang.item.copy(damage = 25)
+    println("pedangBaru: ${pedangBaru.damage}")
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin nakal"))
+    processEvent(BattleState.LootDropped(pedangBaru))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 
 }
