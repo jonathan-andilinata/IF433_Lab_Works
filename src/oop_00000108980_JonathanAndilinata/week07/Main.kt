31224fcd5fad86bf900a4f43d945fbcc2be9f773
jonathan <jonathan07@gmail.com>
2026-03-27 11:01:19 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_00000108980_JonathanAndilinata/week07/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/Main.kt
@@ -2,15 +2,11 @@ package oop_00000108980_JonathanAndilinata.week07
 
 fun main (){
 
-    println("\n=== TEST Data Class ===")
-    val data1 = RegularUser("Alice",22)
-    val data2 = RegularUser("Alice",22)
-    println(data1)
-    println("Sama? ${data1 == data2}")
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
-    val data3 = data1.copy(age = 23)
-    println("hasil copy $data3")
-
-    val (name, age) = data1
-    println("Destructured $name berumur $age")
+    val uiMessage = when(response){
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan Alert: ${response.message}"
+    }
 }
