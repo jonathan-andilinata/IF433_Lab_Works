5e7632e0154df507f2bd35da00c3cb0914d69c9d
jonathan <jonathan07@gmail.com>
2026-04-17 10:24:27 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_00000108980_JonathanAndilinata/week05/CreditCard.kt
@@ -0,0 +1,4 @@
+package oop_00000108980_JonathanAndilinata.week05
+
+class CreditCard {
+}
--- src/oop_00000108980_JonathanAndilinata/week07/GameManager.kt
+++ src/oop_00000108980_JonathanAndilinata/week07/GameManager.kt
@@ -1,17 +1,4 @@
 package oop_00000108980_JonathanAndilinata.week07
 
-object GameManager {
-    var isGameRunning: Boolean = false
-
-    fun startGame(){
-        if(isGameRunning){
-            println("Memulai Game Engine...")
-        }
-        else{
-            isGameRunning = true
-            println("Game sudah berjalan! Mencegah instansiasi ganda")
-        }
-
-    }
-
+class GameManager {
 }
