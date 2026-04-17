ee73f3aa1a5606f62bdacd9f6c3c1a13865a0d5a
jonathan <jonathan07@gmail.com>
2026-04-17 11:05:54 +0700

    week08: implement java interop using git log

--- src/oop_00000108980_JonathanAndilinata/week08/Main.kt
+++ src/oop_00000108980_JonathanAndilinata/week08/Main.kt
@@ -54,6 +54,11 @@ fun main() {
         print(e.message)
     }
 
+    println("\n === TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
 
+    val statusLength = javaResponse!!.length
+
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
 
