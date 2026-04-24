2add9b41f8f1c6f1cf302a07524e890b9d11fba7
jonathan <jonathan07@gmail.com>
2026-04-24 11:51:59 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000108980_JonathanAndilinata/week09/TradingDashboard.kt
+++ src/oop_00000108980_JonathanAndilinata/week09/TradingDashboard.kt
@@ -3,16 +3,20 @@ package oop_00000108980_JonathanAndilinata.week09
 fun main(){
     val tradeHistory = listOf(
         Tradelog("BTC/USDT", "LONG", 20, 15.5, "CLOSED"),
-        Tradelog("ETH/USDT", "SHORT", 10, -5.2, "OPEN"),
+        Tradelog("ETH/USDT", "SHORT", 10, -5.2, "CLOSED"),
         Tradelog("SOL/USDT", "LONG", 5, 42.0, "CLOSED"),
-        Tradelog("BNB/USDT", "SHORT", 25, 8.7, "CLOSED"),
+        Tradelog("BNB/USDT", "SHORT", 25, 8.7, "OPEN"),
         Tradelog("DOT/USDT", "LONG", 3, -2.1, "OPEN"),
         Tradelog("LINK/USDT", "SHORT", 15, 12.3, "CLOSED")
     )
     val closedTrades = tradeHistory.filter{it.status == "CLOSED"}
     val winningTrades = closedTrades.filter{it.roe > 0}
     val losingTrades = closedTrades.filter{it.roe <= 0}
+
     val topPerformerString = winningTrades.sortedByDescending { it.roe }.map{"WIN [BTCUSDT - LONG]: +15.5% ROE (Lev: 20x)"}
     val worstPerformersString = losingTrades.sortedBy { it.roe }.map{"LOSS [ETHUSDT - SHORT]: -5.2% ROE (Lev: 10x)"}
+
+    val uniquePairs = tradeHistory.map{it.pair}.toSet()
+
 
 }
