7a72e73ab79bd7d2c37c2951d431686fb6a2f7cc
jonathan <jonathan07@gmail.com>
2026-05-14 18:02:32 +0700

    week11: (task) implement device search with let

--- src/oop_00000108980_JonathanAndilinata/week11/SmartHomeDashboard.kt
+++ src/oop_00000108980_JonathanAndilinata/week11/SmartHomeDashboard.kt
@@ -29,4 +29,11 @@ fun main() {
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder","Pet Care",true,10))
 
+    val searchResult = homeDevices.find{
+        it.category == "Camera"
+    }?.let{
+        val result = it.diagnose()
+        print(result)
+    }
+
 }
