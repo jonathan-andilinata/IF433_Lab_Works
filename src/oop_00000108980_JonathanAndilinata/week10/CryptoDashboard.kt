05b92c18ff6398aadaf18ef049cbb2077f5b656c
jonathan <jonathan07@gmail.com>
2026-05-14 16:29:35 +0700

    week10: (task) populate coin repository

--- src/oop_00000108980_JonathanAndilinata/week10/CryptoDashboard.kt
+++ src/oop_00000108980_JonathanAndilinata/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000108980_JonathanAndilinata.week10
 
 fun main(){
     val coinRepo = WalletRespitory<Coin>()
+    coinRepo.add(Coin("BTC",1.0))
+    coinRepo.add(Coin("ETH",0.8))
+    coinRepo.add(Coin("USDT",0.54))
 }
