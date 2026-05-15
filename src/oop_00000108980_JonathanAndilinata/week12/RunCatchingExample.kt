0217344a9e27032dc4fdaef2151b01884b7f78b3
jonathan <jonathan07@gmail.com>
2026-05-15 11:38:40 +0700

    week12: implement onSuccess and onFailure chaining

--- src/oop_00000108980_JonathanAndilinata/week12/RunCatchingExample.kt
+++ src/oop_00000108980_JonathanAndilinata/week12/RunCatchingExample.kt
@@ -8,4 +8,11 @@ fun main(){
     val safeValue = result.getOrElse{-1}
     println("Safe Value (getOrElse): $safeValue")
 
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
     }
+}
